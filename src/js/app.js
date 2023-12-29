683dd08c3dd535668a8d6af2841fb6ca36040040
varavin <Slavon1999@ukr.net>
2023-12-29 23:10:48 +0200

    asda

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import * as flsFunctions from "./modules/functions.js";
+import $, { escapeSelector } from "jquery";
 
 flsFunctions.isWebp();
 
@@ -7,39 +8,121 @@ import { Navigation, Pagination } from 'swiper/modules';
 const swiper = new Swiper();
 
 
-//modal
-var modal = document.getElementById('myModal');
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
+$(function () {
 
-btn.onclick = function () {
+   //upward button
+   var btnTop = $('.btn-top')
+   $('.modal').on("scroll", function () {
+      if ($('.modal').scrollTop() >= 20) {
+         btnTop.fadeIn(100);
+      } else {
+         btnTop.fadeOut(100);
+      }
+   });
+   btnTop.on("click", function () {
+      $('.modal').animate({ scrollTop: 0 }, 600);
+   });
 
-   modal.style.display = "block";
-}
-span.onclick = function () {
-   modal.style.display = "none";
-}
-document.addEventListener('keydown', function (e) {
-   if (e.key === 'Escape') {
-      modal.style.display = "none";
-   }
+
+   //open/close modal one
+   var style1 = $('#myModal').css("display");
+   $('#myBtn').on("click", function () {
+      if (style1 == "none") {
+         $('.modal').fadeIn();
+         style1 = "block";
+         $('.modal-two').fadeOut();
+         style2 = "none";
+         $('.modal-three').fadeOut();
+         style3 = "none";
+      } else {
+         $('.modal').fadeOut();
+         style1 = "none";
+
+      }
+
+   });
+   $('.close').on("click", function () {
+      if (style1 == "none") {
+         $('.modal').fadeIn();
+         style1 = "block";
+      } else {
+         $('.modal').fadeOut();
+         style1 = "none";
+      }
+   });
+
+   $(document).on('keydown', function (e) {
+      if (e.key === 'Escape') {
+         $('.modal').fadeOut();
+         style1 = "none";
+      }
+   });
+
+
+   //open/close modal two
+   var style2 = $('#myModal2').css("display");
+   $('#myBtn2').on("click", function () {
+      if (style2 == "none") {
+         $('.modal-two').fadeIn();
+         style2 = "block";
+         $('.modal').fadeOut();
+         style1 = "none";
+         $('.modal-three').fadeOut();
+         style3 = "none";
+      } else {
+         $('.modal-two').fadeOut();
+         style2 = "none";
+      }
+   });
+   $('.close-two').on("click", function () {
+      if (style2 == "none") {
+         $('.modal-two').fadeIn();
+         style2 = "block";
+      } else {
+         $('.modal-two').fadeOut();
+         style2 = "none";
+      }
+   });
+   $(document).on('keydown', function (e) {
+      if (e.key === 'Escape') {
+         $('.modal-two').fadeOut();
+         style2 = "none";
+      }
+   });
+
+
+   //open/close modal three
+   var style3 = $('#myModal3').css("display");
+   $('#myBtn3').on("click", function () {
+      if (style3 == "none") {
+         $('.modal-three').fadeIn();
+         style3 = "block";
+         $('.modal-two').fadeOut();
+         style2 = "none";
+         $('.modal').fadeOut();
+         style1 = "none";
+      } else {
+         $('.modal-three').fadeOut();
+         style3 = "none";
+      }
+   });
+   $('.close-three').on("click", function () {
+      if (style3 == "none") {
+         $('.modal-three').fadeIn();
+         style3 = "block";
+      } else {
+         $('.modal-three').fadeOut();
+         style3 = "none";
+      }
+   });
+   $(document).on('keydown', function (e) {
+      if (e.key === 'Escape') {
+         $('.modal-three').fadeOut();
+         style3 = "none";
+      }
+   });
+
+
 });
 
-//modal 2
-var modalTwo = document.getElementById('myModal2');
-var btnTwo = document.getElementById("myBtn2");
-var spanTwo = document.getElementsByClassName("close-two")[0];
-
-btnTwo.onclick = function () {
-
-   modalTwo.style.display = "block";
-}
-spanTwo.onclick = function () {
-   modalTwo.style.display = "none";
-}
-document.addEventListener('keydown', function (e) {
-   if (e.key === 'Escape') {
-      modalTwo.style.display = "none";
-   }
-});
 
